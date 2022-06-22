21bf728e9821ed8e771eca2f83eb97f1c4cc578b
Kelvin <kelvinsouza369@outlook.com>
2022-06-21 23:05:34 -0300

    :wrench: config: config commitizen, commitlint, husky, lint-staged

--- apps/vite-project/.eslintrc.js
+++ apps/vite-project/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    root: true,
-    extends: ['infojr-ts'],
-    ignorePatterns: ['node_modules', 'dist', '**/*.js'],
-    parserOptions: {
-        project: 'tsconfig.json',
-        tsconfigRootDir: __dirname,
-    },
+  root: true,
+  extends: ['infojr-ts'],
+  ignorePatterns: ['node_modules', 'dist', 'vite.config.ts', '**/*.js'],
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname
+  }
 }
